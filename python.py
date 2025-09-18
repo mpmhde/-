897f2349f94ab17f259b89a3cc809a1a1e1d3b68
mpmhde <pirojoek5@gmail.com>
2025-09-18 14:31:24 +0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,6 @@
+ numbers = [1, 2, 3, 4, 5]
+ fruits = ["apple", "banana", "orange"]
+ mixed = [1, "hello", 3.14, True]
+ print("Числа:", numbers)
+ print("Фрукты:", fruits)
+ print("Смешанный:", mixed)
